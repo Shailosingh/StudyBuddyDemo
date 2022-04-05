53e050438581a57153d2bc5d54896c25ea88fb50
Shailendra Singh <computerface56@hotmail.com>
2022-04-05 18:36:04 -0400

    Added comments.

--- StudyBuddyDemo/MainWindow.xaml.cs
+++ StudyBuddyDemo/MainWindow.xaml.cs
@@ -1,4 +1,26 @@
-﻿using Microsoft.UI.Windowing;
+﻿/*
+------------------------------------------------------------------------------------------
+Project: Study Buddy Demo
+Purpose: A demo of the Study Buddy program.
+==========================================================================================
+Program Description:
+Study Buddy is a focus-based productivity tool that incorporates a minigame to improve the 
+user’s engagement with their own study plan. Its purpose is to help users who struggle with 
+staying on task by introducing a rewards system they can become invested in. In academia in 
+particular, bad grades are a major source of negative reinforcement. The main source of 
+positive reinforcement is gaining skills and experience to help the student in their later 
+career. However, these are usually not immediately noticeable or useful, so most of a student’s 
+motivation comes from the fear of poor and failing grades on assessments. Our aim with this 
+project is to introduce a source of positive reinforcement for users who need more of it in 
+order to succeed in their studies.
+------------------------------------------------------------------------------------------
+Author:  Shailendra Singh, Adam Scott, Riley Huston, Christine Nguyen, Terry Tran, 
+Tatiana Olenciuc, Alex Lau
+Version  2022-04-05
+------------------------------------------------------------------------------------------
+*/
+
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
